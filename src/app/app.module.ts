70b3aad2859d03896aebba5eeef3ec90ed509251
mink <minkyung1435@gmail.com>
2023-03-18 22:26:28 +0900

    백엔드 데이터랑 연동

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { MainComponent } from './main/main.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, MainComponent, MainpageComponent],
+  declarations: [AppComponent, MainpageComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
