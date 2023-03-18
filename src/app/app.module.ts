0e39b8569924119c9b5d178054ba306a21a94612
mink <minkyung1435@gmail.com>
2023-03-18 12:56:37 +0900

    앵귤러 초기설정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
