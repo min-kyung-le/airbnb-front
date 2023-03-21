fe1bb24ca9cef794b978a9bbdd5c55f124ac77a9
mink <minkyung1435@gmail.com>
2023-03-21 21:21:27 +0900

    모양 수정

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -16,21 +16,18 @@ export class MainpageComponent {
   constructor(private mainpageService: MainpageService) {}
 
   ngOnInit() {
-    console.log('====ngOnInit====');
     this.getMenus();
     this.getRooms();
   }
 
   getMenus(): void {
     this.mainpageService.getMenus().subscribe((menus) => {
-      console.log('menus', menus);
       this.menuList = menus.content;
     });
   }
 
   getRooms(): void {
     this.mainpageService.getRooms().subscribe((room) => {
-      console.log('room', room);
       this.roomList = room.content;
     });
   }
